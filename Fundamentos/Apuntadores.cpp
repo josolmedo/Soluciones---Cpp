37c045b4176ff1fdded5981b0dc0906294eb1aea
José Ángel Olmedo Guevara <joseangelolmedoguevara@ciencias.unam.mx>
2025-04-20 21:19:27 -0600

    Agregando apuntadores

---
+++ Fundamentos/Apuntadores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void saltoDeLinea(){
+    cout<<"\n";
+}
+
+int main(){
+
+    saltoDeLinea();
+
+    //Un apuntador (pointer) es una variable que almacena la dirección de memoria de otra variable.
+
+
+
+
+
+    return 0;
+}
