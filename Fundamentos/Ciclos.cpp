4ff429d877318352fcd7a369b7c4ade744e36510
José Olmedo <116040147+josolmedo@users.noreply.github.com>
2025-04-20 20:25:33 -0600

    Ciclos

---
+++ Fundamentos/Ciclos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std; //Permite utilizar cin y cout
+
+int main(){
+
+    int contador = 0;
+
+    for(int i = 0; i < 20; i++){
+        cout<<i<<"\n";
+    }
+
+    while(contador < 20){
+        cout<<"Ésta es la iteración número: "<<contador<<endl;
+        contador++;
+    }
+    contador = 0;
+
+    do {
+        cout << "Iteración: " << contador << endl;
+        contador++;
+    } while (contador <= 5);
+
+
+
+    return 0;
+}
